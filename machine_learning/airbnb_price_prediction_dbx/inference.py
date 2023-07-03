48fa7ff6dea72edbabbc439cadbec301ab3617e6
Manuel Gil <manuelgilsitio@gmail.com>
2023-07-03 16:07:59 -0500

    inference with feature store

--- machine_learning/airbnb_price_prediction_dbx/inference.py
+++ machine_learning/airbnb_price_prediction_dbx/inference.py
@@ -1,49 +1,24 @@
-import mlflow
-
-from pyspark.sql.functions import pandas_udf
-from pyspark.sql import DataFrame
-import pandas as pd 
-
-def get_model(model_uri: str):
-    """
-    This function loads the model from the given model uri.
-
-    params: model_uri: str
-        The model uri.
-    return: model: mlflow.spark.models.PythonModel()
-
-    """
-    return mlflow.pyfunc.spark_udf(model_uri=model_uri)
-
-
-# @pandas_udf("double")
-# def predict_udf(*args:DataFrame) -> DataFrame:
-#     model_uri = 'runs:/ac8576dea85c45458cfb0ee7ddc9b000/pipeline'
-#     model = get_model(model_uri=model_uri)
-#     pdf = pd.concat(args, axis=1)
-#     return pd.Series(model.predict(pdf))
+from databricks.feature_store import FeatureStoreClient
+from utils import util_functions as uf
 
 
 if __name__=="__main__":
 
     fs = FeatureStoreClient()
-    config = get_configurations(filename="feature_preparation")
+    config = uf.get_configurations(filename="feature_preparation")
     experiment_name = config["experiment_name"]
-    categorical_columns, numerical_columns = get_feature_names(config=config)
+    categorical_columns, numerical_columns = uf.get_feature_names(config=config)
     categorical_features = [col + "_indexed" for col in categorical_columns]
     numerical_features = [col + "_imputed" for col in numerical_columns]
 
-    train_ids, test_ids = get_train_test_ids(config=config, fs=fs)
-    training_set, testing_set = get_train_test_sets(
-        config=config,
-        fs=fs,
-        train_ids=train_ids,
-        test_ids=test_ids,
-        feature_names=categorical_columns + numerical_columns,
+    _, test_ids = uf.get_train_test_ids(config=config, fs=fs)
+
+    model_uri =  'runs:/b4c6188e8fc343c79162f90b7e61f1ad/fs_cv_pipeline'
+
+    # When using feature store we only need to pass the ids to the score_batch function.
+    # The feature store will take care of the rest.
+    predictions = fs.score_batch(
+        model_uri=model_uri,
+        df=test_ids.select("id"),
     )
-    train_sdf = training_set.load_df()
-    test_sdf = testing_set.load_df()
-
-    model_uri =  'runs:/ac8576dea85c45458cfb0ee7ddc9b000/pipeline'
-    model = get_model(model_uri=model_uri)
-
+    print(predictions.show())
