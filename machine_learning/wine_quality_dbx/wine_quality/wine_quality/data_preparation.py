cd8f20068591294b98648b9e74e77e6f7adae944
Manuel Gil <manuelgilsitio@gmail.com>
2023-07-08 16:27:00 -0500

    feature table creationg

---
+++ machine_learning/wine_quality_dbx/wine_quality/wine_quality/data_preparation.py
@@ -0,0 +1,51 @@
+from pyspark.sql import DataFrame
+from pyspark.sql import functions as F
+from databricks.feature_store import FeatureStoreClient
+import yaml
+
+def get_dataframe(spark, path: str) -> DataFrame:
+    """
+    This function reads the csv file from the specified path and returns a spark dataframe.
+    params: spark: spark session
+    params: path: path to the csv file
+    return: sdf: spark dataframe
+    """
+    sdf = spark.read.csv(path, sep=";", header=True, inferSchema=True)
+    return sdf
+
+
+def get_configurations(filename: str) -> dict:
+    """Reads the configuration file from dbfs and returns a dictionary.
+
+    params: filename: str
+    return: config: dict
+    """
+    with open(f"/dbfs/FileStore/configs/wine_quality/{filename}.yaml", "rb") as f:
+        print(f"Reading the configuration file from dbfs: {filename}.yaml")
+        config = yaml.load(f)
+    return config
+
+
+def create_feature_table(
+    sdf: DataFrame,
+    database_name: str,
+    feature_table_name: str,
+    feature_table_path: str,
+    description: str,
+) -> None:
+    """
+    This function creates the feature table in delta lake format.
+    params: sdf: spark dataframe
+    """
+
+    fs = FeatureStoreClient()
+
+    fs.create_table(
+        name=f"{database_name}.{feature_table_name}",
+        df=sdf,
+        schema=sdf.schema,
+        primary_keys=["id"],
+        description=description,
+        partition_columns=["id"],
+        path=feature_table_path,
+    )
