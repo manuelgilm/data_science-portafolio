24c876f6c7065e88643deade7108b27bc47d8aba
Manuel Gil <manuelgilsitio@gmail.com>
2024-02-15 07:54:32 -0500

    adding eda

---
+++ classification/iris_classifier/iris_classifier/eda/vz_app.py
@@ -0,0 +1,50 @@
+import matplotlib.pyplot as plt
+import seaborn as sns
+import streamlit as st
+from iris_classifier.data.retrieval import get_dataset
+from iris_classifier.utils.utils import get_project_root
+
+# Assuming you have loaded iris dataset
+df = get_dataset()
+
+# Add your image path
+root_path = get_project_root()
+header_image_path = (
+    root_path / "iris_classifier/eda/iris-dataset.png"
+)  # Change "your_image_path.png" to the actual path of your image
+header_image = plt.imread(header_image_path)
+
+# Title of your web app with an image in the header
+st.title("Exploratory Data Analysis with Iris Dataset")
+st.image(header_image, use_column_width=True)
+
+# Display the dataframe
+st.subheader("Dataframe")
+st.write(df)
+
+# Summary statistics
+st.subheader("Summary Statistics")
+st.write(df.describe())
+
+# Correlation heatmap
+st.subheader("Correlation Heatmap")
+plt.figure(figsize=(8, 6))
+sns.heatmap(df.corr(), annot=True, cmap="coolwarm", fmt=".2f", linewidths=0.5)
+correlation_heatmap = plt.gcf()  # Get the current figure explicitly
+st.pyplot(correlation_heatmap)
+
+# Pairplot
+st.subheader("Pairplot")
+pairplot = sns.pairplot(df, hue="target")
+pairplot_figure = pairplot.fig  # Get the figure from the pairplot explicitly
+st.pyplot(pairplot_figure)
+
+# Bar plot of target variable distribution
+st.subheader("Target Variable Distribution")
+plt.figure(figsize=(8, 4))
+sns.countplot(x="target", data=df)
+plt.xlabel("Target Class")
+plt.ylabel("Count")
+plt.title("Distribution of Target Variable")
+target_distribution_plot = plt.gcf()  # Get the current figure explicitly
+st.pyplot(target_distribution_plot)
