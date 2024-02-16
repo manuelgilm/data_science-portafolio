dbcd2ba991e61bfc47ed50c21fc5244455533a78
Manuel Gil <manuelgilsitio@gmail.com>
2024-02-16 07:28:32 -0500

    adding prediction code

---
+++ classification/iris_classifier/iris_classifier/entrypoints/predict.py
@@ -0,0 +1,24 @@
+from iris_classifier.data.retrieval import get_train_test_data
+from iris_classifier.inference.scoring import get_latest_run_id
+from iris_classifier.inference.scoring import get_predictions
+
+
+def predict():
+    """
+    Predict using the iris classifier.
+    """
+    config = {
+        "experiment_name": "iris_classifier",
+        "model_artifact": "iris_classifier",
+    }
+    x_train, x_test, y_train, y_test = get_train_test_data()
+    run_id = get_latest_run_id(config["experiment_name"])
+    predictions = get_predictions(run_id, x_test, config)
+    map_predictions = {0: "setosa", 1: "versicolor", 2: "virginica"}
+    predictions = [map_predictions[pred] for pred in predictions]
+    pred_df = x_test.copy()
+    pred_df["target"] = y_test
+    pred_df["target_mapped"] = pred_df["target"].map(map_predictions)
+    pred_df["prediction"] = predictions
+
+    print(pred_df.head(10))
---
+++ classification/iris_classifier/iris_classifier/inference/scoring.py
@@ -0,0 +1,37 @@
+from typing import Any
+from typing import Dict
+
+import mlflow
+import pandas as pd
+
+
+def get_predictions(run_id: str, x_test: pd.DataFrame, config: Dict[str, Any]):
+    """
+    Get predictions from the iris classifier.
+
+    :param run_id: The run id of the model to use for predictions.
+    :param x_test: The test data to use for predictions.
+    :param config: The configuration for the model.
+    :return: The predictions from the iris classifier.
+    """
+
+    model_uri = f"runs:/{run_id}/{config['model_artifact']}"
+    loaded_model = mlflow.sklearn.load_model(model_uri)
+    predictions = loaded_model.predict(x_test)
+    return predictions
+
+
+def get_latest_run_id(experiment_name: str):
+    """
+    Get the latest run id from the experiment.
+
+    :param experiment_name: The name of the experiment.
+    :return: The latest run id from the experiment.
+    """
+    experiment = mlflow.get_experiment_by_name(experiment_name)
+    latest_run = mlflow.search_runs(
+        experiment_ids=experiment.experiment_id,
+        order_by=["start_time desc"],
+        max_results=1,
+    )
+    return latest_run.iloc[0]["run_id"]
