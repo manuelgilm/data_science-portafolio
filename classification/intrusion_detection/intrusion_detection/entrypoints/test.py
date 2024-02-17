2151d27bf47617f23201f9aba467e8f164385095
Manuel Gil <manuelgilsitio@gmail.com>
2024-02-17 10:30:09 -0500

    getting data

---
+++ classification/intrusion_detection/intrusion_detection/entrypoints/test.py
@@ -0,0 +1,8 @@
+from intrusion_detection.data.retrieval import get_dataset
+
+
+def get_data():
+    """
+    Get the dataset from the retrieval module and print it.
+    """
+    return print(get_dataset())
