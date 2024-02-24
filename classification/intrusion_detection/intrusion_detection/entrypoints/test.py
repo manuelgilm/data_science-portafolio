333d0a39cdc1a3d7f440e6ba443ca62751c6852b
Manuel Gil <manuelgilsitio@gmail.com>
2024-02-24 09:09:04 -0500

    deleting intrucsion detection

--- classification/intrusion_detection/intrusion_detection/entrypoints/test.py
+++
@@ -1,8 +0,0 @@
-from intrusion_detection.data.retrieval import get_dataset
-
-
-def get_data():
-    """
-    Get the dataset from the retrieval module and print it.
-    """
-    return print(get_dataset())
